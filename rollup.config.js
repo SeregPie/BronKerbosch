b096d6acc509d213304b31c4a80f895e2b7f8bed
Sergej Sintschilin <seregpie@gmail.com>
2021-06-25 13:08:30 +0200

    use babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
+import {babel} from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
-import buble from '@rollup/plugin-buble';
 
 import {main} from './package.json';
 
 export default {
 	input: 'src/index.js',
 	plugins: [
-		buble(),
+		babel({babelHelpers: 'bundled'}),
 		terser(),
 	],
 	output: {
