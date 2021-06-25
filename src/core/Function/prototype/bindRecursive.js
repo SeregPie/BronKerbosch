3e8053bc92446ff37a0c0ea26970d810a5dbc69d
Sergej Sintschilin <seregpie@gmail.com>
2021-06-25 13:22:37 +0200

    refactor

--- src/core/Function/prototype/bindRecursive.js
+++
@@ -1,6 +0,0 @@
-export default function(that) {
-	let recur = function(...args) {
-		return that.call(this, recur, ...args);
-	};
-	return recur;
-}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import Function_prototype_bindRecursive from './core/Function/prototype/bindRecursive';
-import Set_difference from './core/Set/difference';
-import Set_intersection from './core/Set/intersection';
-import Set_union from './core/Set/union';
+import difference from './utils/difference';
+import intersection from './utils/intersection';
+import union from './utils/union';
 
 export default function(edges) {
 	edges = Array.from(edges);
@@ -21,23 +20,28 @@ export default function(edges) {
 		neighbors.get(edge[1]).add(edge[0]);
 	});
 	let cliques = [];
-	Function_prototype_bindRecursive((recur, clique, candidates, excludedCandidates) => {
+	let f = ((clique, candidates, excludedCandidates) => {
 		if (!candidates.size && !excludedCandidates.size) {
 			cliques.push(Array.from(clique));
 		}
 		let pivotNeighbors = new Set();
-		Set_union(candidates, excludedCandidates).forEach(candidate => {
-			let t = Set_intersection(neighbors.get(candidate), candidates);
+		union(candidates, excludedCandidates).forEach(candidate => {
+			let t = intersection(neighbors.get(candidate), candidates);
 			if (t.size > pivotNeighbors.size) {
 				pivotNeighbors = t;
 			}
 		});
-		Set_difference(candidates, pivotNeighbors).forEach(candidate => {
+		difference(candidates, pivotNeighbors).forEach(candidate => {
 			let candidateNeighbors = neighbors.get(candidate);
-			recur((new Set(clique)).add(candidate), Set_intersection(candidates, candidateNeighbors), Set_intersection(excludedCandidates, candidateNeighbors));
+			f(
+				(new Set(clique)).add(candidate),
+				intersection(candidates, candidateNeighbors),
+				intersection(excludedCandidates, candidateNeighbors),
+			);
 			candidates.delete(candidate);
 			excludedCandidates.add(candidate);
 		});
-	})(new Set(), nodes, new Set());
+	});
+	f(new Set(), nodes, new Set());
 	return cliques;
 }
