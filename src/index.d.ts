2031abf719914893201aea085950eed40d0680cc
Sergej Sintschilin <seregpie@gmail.com>
2023-10-06 17:00:27 +0200

    wip-2

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,26 @@
-export default function runBronKerbosch<T>(edges: Iterable<Readonly<[T, T]>>): Array<Array<T>>;
+export default function runBronKerbosch<T>(
+	edges: Iterable<Readonly<[T, T]>>,
+): Array<Array<T>>;
+
+export type KMeansOptions<T> = {
+	distance: {(a: T, b: T): number};
+	mean: {(...nodes: ReadonlyArray<T>): T};
+	maxIterations: number;
+	map: {};
+	random: {(): number};
+};
+
+export function runKMeans<T>(
+	nodes: Iterable<T>,
+	means: number | Iterable<T>,
+	options: KMeansOptions<T>,
+): Array<Array<T>>;
+
+export type NearestNeighborChainCluster<T> = Array<
+	T | NearestNeighborChainCluster<T>
+>;
+
+export function runNearestNeighborChain<T>(
+	nodes: Iterable<T>,
+	distance: {(a: T, b: T): number},
+): NearestNeighborChainCluster<T>;
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -10,7 +10,6 @@ describe('runBronKerbosch', () => {
 	test('should work', () => {
 		let edges: Array<[number, number]> = [[1, 2], [1, 5], [2, 5], [3, 4], [4, 5], [4, 6]];
 		let cliques = runBronKerbosch(edges);
-		console.log(cliques);
 		expect(cliques).toEqual([[3, 4], [4, 5], [4, 6], [1, 2, 5]]);
 	});
 
