262260465b01183c20c14ef359905a878d5dc625
Sergej Sintschilin <seregpie@gmail.com>
2023-10-24 17:48:06 +0200

    +

--- src/KMeans/index.js
+++ src/KMeans/index.js
@@ -3,17 +3,54 @@ export default (
 	centers,
 	calcDistance,
 	calcCenter,
-	{random = Math.random} = {},
+	{
+		//
+		iterations = 1024,
+		random = Math.random,
+	} = {},
 ) => {
 	items = Array.from(items);
-	let itemsCount = items.length;
-	let centersType = typeof center;
-	let centersCount = centersType;
-	Number.isFinite;
 	centers = ((v) => {
-		let type = typeof v;
+		if (typeof v === 'number') {
+			centers = Array.from({length: centers}, () => 0);
+		}
+		return Array.from(v);
 	})(centers);
-	if (centers.length > 0) {
+
+	luck.pickCombination(values, clustersCount);
+	if (typeof centers === 'number') {
+		centers = Array.from({length: centers}, () => 0);
 	}
-	return [];
+	let assignments = items.map(() => -1);
+	item = {
+		cluster: null | cluster,
+	};
+	cluster = {
+		center,
+		items,
+	};
+	if (items.length > 0 && centers.length > 0) {
+		for (let i = 0; i < iterations; i++) {
+			let stop = true;
+			items.forEach((item, i) => {
+				let v = centers.findIndexOfMin((center) => calcDistance(item, center));
+				if (assignments[i] !== v) {
+					assignments[i] = v;
+					stop = false;
+				}
+			});
+			if (stop) break;
+			centers = centers.map((center, i) => {
+				let items = clusters[i];
+				switch (items.length) {
+					case 0:
+						return center;
+					case 1:
+						return items[0];
+				}
+				return calcCenter(...items);
+			});
+		}
+	}
+	return clusters;
 };
